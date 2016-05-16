9a5dc8f3c4aac12901dec1ceb4670038d59e14a8
Rodrigo Okido <rodrigo_okido@hotmail.com>
2016-05-15 22:49:23 -0300

    Protótipo

---
+++ TetrisGame/src/tetrisInterface/TetrisApp.java
@@ -0,0 +1,12 @@
+package tetrisInterface;
+
+import javax.swing.JFrame;
+
+public class TetrisApp extends JFrame{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
